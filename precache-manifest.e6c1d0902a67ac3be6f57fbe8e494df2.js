97c34ee45af8228b5022b64ebc64a7f2f0d32a80
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-07 00:46:07 -0300

    e519bd3

---
+++ precache-manifest.e6c1d0902a67ac3be6f57fbe8e494df2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Vue_Tdd/robots.txt"
+  },
+  {
+    "revision": "b8c07dfb625829ba5758",
+    "url": "/Vue_Tdd/js/chunk-vendors.786b3827.js"
+  },
+  {
+    "revision": "92130d74ac49f207cc97",
+    "url": "/Vue_Tdd/js/app.ed3af9d8.js"
+  },
+  {
+    "revision": "5473f55e7d44d6b157c645c65a3a5158",
+    "url": "/Vue_Tdd/index.html"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/Vue_Tdd/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/Vue_Tdd/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "b8c07dfb625829ba5758",
+    "url": "/Vue_Tdd/css/chunk-vendors.d9890480.css"
+  },
+  {
+    "revision": "92130d74ac49f207cc97",
+    "url": "/Vue_Tdd/css/app.48547cf6.css"
+  }
+];
